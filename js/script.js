fe22078162be1818feee0e87222635fb218789ba
Danilo Teixeira dos Santos <daniloteixeira-santos@hotmail.com>
2013-03-24 12:15:33 -0300

    adicionando suporte para Firefox

--- js/script.js
+++ js/script.js
@@ -191,15 +191,16 @@
 
 	}
 
+	/**
+	 * requestAnimationFrame polyfill by Erik Möller
+	 * Fixes from Paul Irish and Tino Zijdel
+	 *
+	 * @see http://goo.gl/ZC1Lm
+	 * @see http://goo.gl/X0h6k
+	 */
+	(function(){for(var d=0,a=["ms","moz","webkit","o"],b=0;b<a.length&&!window.requestAnimationFrame;++b)window.requestAnimationFrame=window[a[b]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[a[b]+"CancelAnimationFrame"]||window[a[b]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(window.requestAnimationFrame=function(b){var a=Date.now(),c=Math.max(0,16-(a-d)),e=window.setTimeout(function(){b(a+c)},c);d=a+c;return e});window.cancelAnimationFrame||(window.cancelAnimationFrame=function(a){clearTimeout(a)})})();
+	
 	init();
 
 }( window, document );
 
-/**
- * requestAnimationFrame polyfill by Erik Möller
- * Fixes from Paul Irish and Tino Zijdel
- *
- * @see http://goo.gl/ZC1Lm
- * @see http://goo.gl/X0h6k
- */
-(function(){for(var d=0,a=["ms","moz","webkit","o"],b=0;b<a.length&&!window.requestAnimationFrame;++b)window.requestAnimationFrame=window[a[b]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[a[b]+"CancelAnimationFrame"]||window[a[b]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(window.requestAnimationFrame=function(b){var a=Date.now(),c=Math.max(0,16-(a-d)),e=window.setTimeout(function(){b(a+c)},c);d=a+c;return e});window.cancelAnimationFrame||(window.cancelAnimationFrame=function(a){clearTimeout(a)})})();
